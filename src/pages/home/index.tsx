612626efffcf196e3ebee923640f91f26efadc8c
Joao Marcos <joaomarcoslferraz@gmail.com>
2026-02-25 18:24:03 -0300

    adicao de boas vindas na home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,12 +4,17 @@ import { Cards } from "../../components/cads"
 export function Home (){
     return(
         <>
-        <div className="flex justify-center items-center px-16 py-16 bg-zinc-600 min-h-screen gap-8">
-        <NavLink to="/taskMaster" ><Cards titulo="Task Master" descricao= "Adição, listagem e remoção de tarefas"></Cards></NavLink>
-        <NavLink to="/connectHub"><Cards titulo="Connect Hub" descricao="Cadastro de novos contatos"></Cards></NavLink>
-        <NavLink to="/moneyFlow"><Cards titulo="Money Flow" descricao="Registro de entradas e saídas"></Cards></NavLink>
+        <div className="flex flex-col items-center px-16 py-16 bg-zinc-600 min-h-screen">
+            <div className="flex flex-col items-center mb-10">
+                <img src="/logo.png" alt="Logo QuickTools" className="w-12 h-12 mb-2" />
+                <h2 className="text-2xl font-bold text-white">Boas vindas ao QuickTools</h2>
+            </div>
+            <div className="flex justify-center items-center gap-8">
+                <NavLink to="/taskMaster" ><Cards titulo="Task Master" descricao= "Adição, listagem e remoção de tarefas"></Cards></NavLink>
+                <NavLink to="/connectHub"><Cards titulo="Connect Hub" descricao="Cadastro de novos contatos"></Cards></NavLink>
+                <NavLink to="/moneyFlow"><Cards titulo="Money Flow" descricao="Registro de entradas e saídas"></Cards></NavLink>
+            </div>
         </div>
         </>
-        
     )
 }
